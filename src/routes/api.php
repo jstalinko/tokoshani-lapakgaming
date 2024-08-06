4f59f4c41fb78a21a290a7ef2ff5ee459c6c9ef0
jstalinko <alinkodotid@gmail.com>
2024-08-06 12:55:21 +0700

    fix: routes categories

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,10 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Jstalinko\TokoshaniLapakgaming\Http\Controllers\VipresellerController;
+use Jstalinko\TokoshaniLapakgaming\Http\Controllers\LapakgamingController;
 
 Route::group(['prefix' => 'shn-api'], function () {
     Route::group(['prefix' => 'v2'] , function(){
-        Route::get('/categories' ,[] );
+        Route::get('/categories' ,[LapakgamingController::class , 'getCategory'])->name('tokoshani.lapakgaming.getcategories');
     });
 });
